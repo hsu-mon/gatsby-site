db0999b1b66ece2b59e95f7bb6a6b399d2c2e1aa
hsu-mon <myathsumon3012@gmail.com>
2021-11-18 13:44:32 +0630

    add chargebee subscription

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,17 @@ const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Starter Plan</p>
+    <p>
+      <Link to="https://myathsu-test.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=Starter-USD-Yearly" target="_blank">Advanced</Link>
+    </p>
+    <p>Customer Portal</p>
+    <p>
+      <Link to="https://myathsu-test.chargebeeportal.com" target="_blank">Manage Your Subscription</Link>
+    </p>
+    <p>
+      <Link to="/">Go back to the homepage</Link>
+    </p>
   </Layout>
 )
 
